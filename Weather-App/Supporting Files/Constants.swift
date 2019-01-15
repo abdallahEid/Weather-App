7b2ccf3471004e46fca9b5ef8cbbb8ad97094c20
abdullahEidM <abdallah.eid@seamlabs.com>
2019-01-15 22:46:14 +0200

    Organizing the basic structure of the project

---
+++ Weather-App/Supporting Files/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Weather-App
+//
+//  Created by Abdallah Eid on 1/15/19.
+//  Copyright © 2019 Abdallah Eid. All rights reserved.
+//
+
+/*
+ This class contains the constants to be used in the project to reduce using hard coded
+ values in the project.
+ */
+
+import Foundation
+
+class Constants {
+    
+}
+
---
+++ Weather-App/Supporting Files/Functions.swift
@@ -0,0 +1,17 @@
+//
+//  Functions.swift
+//  Weather-App
+//
+//  Created by Abdallah Eid on 1/15/19.
+//  Copyright © 2019 Abdallah Eid. All rights reserved.
+//
+
+/*
+ This class contains the Common Function to be used in any place in the project
+*/
+
+import Foundation
+
+class Functions {
+    
+}
